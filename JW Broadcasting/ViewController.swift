15f59c0b0870abdf06c3323e53b0051f72933fb3
Austin <austinz@mac.com>
2015-09-13 23:41:53 -0700

    Initial Commit

---
+++ JW Broadcasting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JW Broadcasting
+//
+//  Created by Austin Zelenka on 9/13/15.
+//  Copyright © 2015 xquared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JW BroadcastingTests/JW_BroadcastingTests.swift
@@ -0,0 +1,36 @@
+//
+//  JW_BroadcastingTests.swift
+//  JW BroadcastingTests
+//
+//  Created by Austin Zelenka on 9/13/15.
+//  Copyright © 2015 xquared. All rights reserved.
+//
+
+import XCTest
+@testable import JW_Broadcasting
+
+class JW_BroadcastingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
