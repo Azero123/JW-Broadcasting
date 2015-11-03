6fae82981426f2ec1e90587b518c76dce8613417
Austin <austinz@mac.com>
2015-11-02 17:01:31 -0800

    made it white and changed some looks… more to come

--- JW Broadcasting/VideoOnDemandController.swift
+++ JW Broadcasting/VideoOnDemandController.swift
@@ -18,7 +18,7 @@ class VideoOnDemandController: UIViewController, UITableViewDelegate, UITableVie
         super.viewDidLoad()
 
         //http://mediator.jw.org/v1/categories/ASL/VideoOnDemand?detailed=1
-        
+        self.videoCollection.clipsToBounds=false
         renewContent()
     
     }
@@ -60,12 +60,13 @@ class VideoOnDemandController: UIViewController, UITableViewDelegate, UITableVie
         
         let category:UITableViewCell=tableView.dequeueReusableCellWithIdentifier("category", forIndexPath: indexPath)
         category.textLabel?.text=subcats.objectAtIndex(indexPath.row).objectForKey("name") as? String
-        category.textLabel?.textColor=UIColor.whiteColor()
+        //category.textLabel?.textColor=UIColor.whiteColor()
+        
         return category
     }
     
     func tableView(tableView: UITableView, didUpdateFocusInContext context: UITableViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
-        
+        /*
         if (context.previouslyFocusedView != nil && (context.previouslyFocusedView?.isKindOfClass(UITableViewCell.self) == true) ){
         
             (context.previouslyFocusedView as! UITableViewCell).textLabel?.textColor=UIColor.whiteColor()
@@ -75,7 +76,7 @@ class VideoOnDemandController: UIViewController, UITableViewDelegate, UITableVie
             
             (context.nextFocusedView as! UITableViewCell).textLabel?.textColor=UIColor.blackColor()
             
-        }
+        }*/
     }
     
     func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
@@ -98,18 +99,35 @@ class VideoOnDemandController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSizeMake(270, 270)
+        return CGSizeMake(270, 320)
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell: UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("video", forIndexPath: indexPath)
         
+        for subview in cell.contentView.subviews {
+            subview.removeFromSuperview()
+        }
+        
         let retrievedVideo=parentCategory.objectAtIndex(indexPath.section).objectForKey("media")?.objectAtIndex(indexPath.row)
         
         let image=UIImageView(image: imageUsingCache((retrievedVideo!.objectForKey("images")!.objectForKey("sqr")?.objectForKey("lg"))! as! String))
         
         cell.contentView.addSubview(image)
+        
+        let label=UILabel(frame: CGRect(x: 0, y: 270, width: 270, height: 50))
+        label.text=retrievedVideo!.objectForKey("title") as? String
+        label.textAlignment = .Center
+        label.font=UIFont.systemFontOfSize(30)
+        cell.contentView.addSubview(label)
+        
+        image.layer.shadowColor=UIColor.blackColor().CGColor
+        image.layer.shadowOpacity=1
+        image.layer.shadowRadius=5
+        
+        print(retrievedVideo)
+        
         return cell
     }
     
@@ -121,22 +139,29 @@ class VideoOnDemandController: UIViewController, UITableViewDelegate, UITableVie
         
         */
         
+        
             if (context.previouslyFocusedView != nil && (context.previouslyFocusedView?.isKindOfClass(UICollectionViewCell.self) == true) ){
                 
                 //Clear shadow on any possible previous selection.
                 
                 context.previouslyFocusedView?.layer.shadowColor=UIColor.clearColor().CGColor
-                context.previouslyFocusedView?.layer.shadowOpacity=0
-                context.previouslyFocusedView?.layer.shadowRadius=0
+                context.previouslyFocusedView?.layer.shadowOpacity=1
+                context.previouslyFocusedView?.layer.shadowRadius=5
+                context.previouslyFocusedView?.layer.shadowOffset=CGSize(width: 0, height: 0)
+                context.previouslyFocusedView?.transform=CGAffineTransformScale(CGAffineTransformIdentity, 1, 1)
+                //context.previouslyFocusedView?.frame=CGRect(x: (context.previouslyFocusedView?.frame.origin.x)!, y: (context.previouslyFocusedView?.frame.origin.y)!+40, width: (context.previouslyFocusedView?.frame.size.width)!, height: (context.previouslyFocusedView?.frame.size.height)!)
             }
             
             if ((context.nextFocusedView != nil) && (context.nextFocusedView?.isKindOfClass(UICollectionViewCell.self) == true) ){
                 
                 //Create shadow on newly selected item.
                 
-                context.nextFocusedView?.layer.shadowColor=UIColor.blueColor().CGColor
+                context.nextFocusedView?.layer.shadowColor=UIColor.blackColor().CGColor
                 context.nextFocusedView?.layer.shadowOpacity=1
+                context.nextFocusedView?.layer.shadowOffset=CGSize(width: 0, height: 20)
+                context.nextFocusedView?.transform=CGAffineTransformScale(CGAffineTransformIdentity, 1.2, 1.2)
                 context.nextFocusedView?.layer.shadowRadius=20
+                //context.nextFocusedView?.frame=CGRect(x: (context.nextFocusedView?.frame.origin.x)!, y: (context.nextFocusedView?.frame.origin.y)!-40, width: (context.nextFocusedView?.frame.size.width)!, height: (context.nextFocusedView?.frame.size.height)!)
             }
         return true
     }
